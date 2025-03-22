2dc9e13e8265076725592f21b39cdbb9b6d5b4b4
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-03-22 23:54:33 +0800

    Seperate the column and tasks into componenent

---
+++ client/src/components/Column.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import TaskCard from "./TaskCard";
+
+const Column = ({ title, status, tasks, moveTask }) => {
+  const filteredTasks = tasks.filter((task) => task.status === status);
+  return (
+    <div className="bg-[#DEDEDE] w-[470px] h-[945px] rounded-md shadow-lg p-4 flex flex-col">
+      <div className="flex justify-center mb-4">
+        <div className="bg-[#B4B4B4] text-center w-40 p-2 rounded-md text-white">
+          <p className="text-2xl">{title}</p>
+        </div>
+      </div>
+
+      {filteredTasks.map((task) => (
+        <TaskCard key={task.id} task={task} moveTask={moveTask} />
+      ))}
+    </div>
+  );
+};
+
+export default Column;
---
+++ client/src/components/TaskCard.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const TaskCard = ({ task, moveTask }) => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex gap-2">
+        {task.status !== "todo" && (
+          <button
+            className="bg-gray-300"
+            onClick={() => moveTask(task.id, "todo")}
+          >
+            Todo
+          </button>
+        )}
+        {task.status !== "in-progress" && (
+          <button
+            className="bg-gray-300"
+            onClick={() => moveTask(task.id, "in-progress")}
+          >
+            In Progress
+          </button>
+        )}
+        {task.status !== "done" && (
+          <button
+            className="bg-gray-300"
+            onClick={() => moveTask(task.id, "done")}
+          >
+            Done
+          </button>
+        )}
+      </div>
+      <div>
+        <h1>{task.title}</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore illo
+          earum quae omnis rerum eveniet odit tempora soluta odio atque quia,
+          itaque ea aspernatur cupiditate, vel, velit voluptatem veniam
+          explicabo?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TaskCard;
