30ebbbad422f3259486d27a9616454098c60991f
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2025-04-01 14:53:43 +0800

    Fix dependencies

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const TaskCard = ({ task, moveTask }) => {
   return (
     <div className="flex justify-between">
       <div className="flex gap-2">
-        {task.status !== 'todo' && (
+        {task.status !== "todo" && (
           <button
             className="bg-gray-300"
-            onClick={() => moveTask(task.id, 'todo')}
+            onClick={() => moveTask(task.id, "todo")}
           >
             Todo
           </button>
         )}
-        {task.status !== 'in-progress' && (
+        {task.status !== "in-progress" && (
           <button
             className="bg-gray-300"
-            onClick={() => moveTask(task.id, 'in-progress')}
+            onClick={() => moveTask(task.id, "in-progress")}
           >
             In Progress
           </button>
         )}
-        {task.status !== 'done' && (
+        {task.status !== "done" && (
           <button
             className="bg-gray-300"
-            onClick={() => moveTask(task.id, 'done')}
+            onClick={() => moveTask(task.id, "done")}
           >
             Done
           </button>
@@ -31,12 +31,6 @@ const TaskCard = ({ task, moveTask }) => {
       </div>
       <div>
         <h1>{task.title}</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore illo
-          earum quae omnis rerum eveniet odit tempora soluta odio atque quia,
-          itaque ea aspernatur cupiditate, vel, velit voluptatem veniam
-          explicabo?
-        </p>
       </div>
     </div>
   );
